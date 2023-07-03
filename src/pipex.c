13f254147fe1afadbd47423c0855fccfe197f4d6
Hugo Flohil de Sousa <hugoflohildesousa@gmail.com>
2023-07-03 14:23:55 +0200

    pipes hur tmy brain

--- src/pipex.c
+++ src/pipex.c
@@ -13,17 +13,43 @@
 #include "../include/pipex.h"
 #include <stdio.h>
 #include <unistd.h>
+#include <fcntl.h>
+#include <sys/wait.h>
+
+//int parent(){
+//
+//};
+//
+
+int child(int *fd_pipe, int fd_infile, char *cmd){
+    dup2()
+}
 
 int main(int argc, char *argv[])
 {
-    int fd_[2];
-    char test[5];
+    int fd_pipe[2];
+    int process_id;
+    int fd_infile;
+    int fd_outfile;
 
-    if (pipe(fd_) < 0)
+    if (argc < 4 || argc > 5)
+        return (0);
+
+    //opening files
+    fd_infile = open(argv[1], O_RDONLY);
+    fd_infile = open(argv[4], O_WRONLY | O_CREAT, 0644);
+    //pipe into fd_pipe, check for failure
+    if (pipe(fd_pipe) < 0)
         return (1);
-
-    write(fd_[1], "test", 4);
-    read(fd_[0], test, 4);
-    printf("%s\n", test);
+    //fork process
+    process_id = fork();
+    //child process (file1 cmd1)
+    if (process_id == 0)
+        child(fd_pipe, fd_infile, argv[2]);
+    //parent process (cmd2 file2
+    else {
+        wait(NULL);
+        printf("parent");
+    }
     return (0);
 }
