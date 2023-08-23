8e3cbf64774c7283d10f5e5b07264fde71a5a8f8
Hugo <hugoflohildesousa@gmail.com>
2023-08-23 16:48:26 +0200

    norm

--- include/pipex.h
+++ include/pipex.h
@@ -6,7 +6,7 @@
 /*   By: hflohil- <hflohil-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/23 16:38:07 by hflohil-          #+#    #+#             */
-/*   Updated: 2023/06/23 16:38:08 by hflohil-         ###   ########.fr       */
+/*   Updated: 2023/08/23 15:18:34 by hflohil-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@
 void    free_all(char **exec_args);
 int     count_args(char *cmd_buffer);
 char    *get_root_dir(char *arg1);
-char    **get_args(char *cmd_buffer);
+char    **get_args(char *cmd_buffer, int count, int i);
 int     child(int *fd_pipe, int fd_infile, char *cmd_buffer);
 int     parent(int *fd_pipe, int fd_outfile, char *cmd_buffer);
 
--- src/pipex.c
+++ src/pipex.c
@@ -6,66 +6,71 @@
 /*   By: hflohil- <hflohil-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/23 16:37:59 by hflohil-          #+#    #+#             */
-/*   Updated: 2023/06/23 16:38:22 by hflohil-         ###   ########.fr       */
+/*   Updated: 2023/08/23 15:18:23 by hflohil-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/pipex.h"
 #include "../libft/libft.h"
-#include <stdio.h>
-#include <unistd.h>
 #include <fcntl.h>
+#include <stdio.h>
 #include <sys/wait.h>
+#include <unistd.h>
 
-int child(int *fd_pipe, int fd_infile, char *cmd_buffer){
-    char **exec_args;
-
-    exec_args = get_args(cmd_buffer);
-    printf("child process (infile) %s %s\n", exec_args[0], exec_args[1]);
-    close(fd_pipe[0]);
-    dup2(fd_infile, 0);
-    dup2(fd_pipe[1], 1);
-    if (execve(exec_args[0], exec_args, NULL) == -1) {
-        perror(exec_args[0]);
-        exit(1);
-    }
-    return (0);
-}
-
-int parent(int *fd_pipe, int fd_outfile, char *cmd_buffer){
-    char **exec_args;
-
-    exec_args = get_args(cmd_buffer);
-    printf("parent process (outfile) %s %s\n", exec_args[0], exec_args[1]);
-    close(fd_pipe[1]);
-    dup2(fd_pipe[0], 0);
-    dup2(fd_outfile, 1);
-    if (execve(exec_args[0], exec_args, NULL) == -1) {
-        perror(exec_args[0]);
-        exit(1);
-    }
-    return (0);
-}
-
-int main(int argc, char *argv[])
+int	child(int *fd_pipe, int fd_infile, char *cmd_buffer)
 {
-    int fd_pipe[2];
-    int process_id;
-    int fd_infile;
-    int fd_outfile;
+	char	**exec_args;
 
-    if (argc != 5)
-        return (0);
-    fd_infile = open(argv[1], O_RDONLY);
-    fd_outfile = open(argv[4], O_WRONLY | O_CREAT, 0644);
-    if (pipe(fd_pipe) < 0)
-        return (1);
-    process_id = fork();
-    if (process_id == 0)
-        child(fd_pipe, fd_infile, argv[2]);
-    else {
-        wait(NULL);
-        parent(fd_pipe, fd_outfile, argv[3]);
-    }
-    return (0);
+	exec_args = get_args(cmd_buffer, count_args(cmd_buffer), 0);
+	printf("child process (infile) %s %s\n", exec_args[0], exec_args[1]);
+	close(fd_pipe[0]);
+	dup2(fd_infile, 0);
+	dup2(fd_pipe[1], 1);
+	if (execve(exec_args[0], exec_args, NULL) == -1)
+	{
+		perror(exec_args[0]);
+		exit(1);
+	}
+	return (0);
+}
+
+int	parent(int *fd_pipe, int fd_outfile, char *cmd_buffer)
+{
+	char	**exec_args;
+
+	exec_args = get_args(cmd_buffer, count_args(cmd_buffer), 0);
+	printf("parent process (outfile) %s %s\n", exec_args[0], exec_args[1]);
+	close(fd_pipe[1]);
+	dup2(fd_pipe[0], 0);
+	dup2(fd_outfile, 1);
+	if (execve(exec_args[0], exec_args, NULL) == -1)
+	{
+		perror(exec_args[0]);
+		exit(1);
+	}
+	return (0);
+}
+
+int	main(int argc, char *argv[])
+{
+	int	fd_pipe[2];
+	int	process_id;
+	int	fd_infile;
+	int	fd_outfile;
+
+	if (argc != 5)
+		return (0);
+	fd_infile = open(argv[1], O_RDONLY);
+	fd_outfile = open(argv[4], O_WRONLY | O_CREAT, 0644);
+	if (pipe(fd_pipe) < 0)
+		return (1);
+	process_id = fork();
+	if (process_id == 0)
+		child(fd_pipe, fd_infile, argv[2]);
+	else
+	{
+		wait(NULL);
+		parent(fd_pipe, fd_outfile, argv[3]);
+	}
+	return (0);
 }
